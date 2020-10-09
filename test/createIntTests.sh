21fef236ebf415bf31f5dcfad80af8b3b8543c80
Bob Yantosca <yantosca@seas.harvard.edu>
2020-10-09 17:36:26 -0400

    Bug fix: Change start & end date for int test beginning at 20190101
    Some run directories have simulations starting at 20190701 000000
    and others start at 20190101 000000.  Now replace the default
    1-month simulation to 20 minutes for integration tests in
    the test/createIntTests.sh script.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- test/createIntTests.sh
+++ test/createIntTests.sh
@@ -38,7 +38,8 @@ THIS_DIR=`pwd -P`
 TEST_ROOT=${1}
 
 # sed string editor commands
-SED_INPUT_GEOS="s/20190801 000000/20190701 002000/"
+SED_INPUT_GEOS_1="s/20190801 000000/20190701 002000/"
+SED_INPUT_GEOS_2="s/20190201 000000/20190101 002000/"
 SED_HISTORY_RC="s/00000100 000000/00000000 002000/"
 
 # Remove run directories in the test folder
@@ -63,61 +64,71 @@ cd $GCC_RUN_DIR
 RUNDIR="gc_4x5_standard_merra2"
 CMD="1\n1\n1\n1\n1\n${TEST_ROOT}\n${RUNDIR}\nn\n"
 printf ${CMD} | ./createRunDir.sh >> ${LOG} 2>&1
-sed -i -e "${SED_INPUT_GEOS}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_1}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_2}" ${TEST_ROOT}/${RUNDIR}/input.geos
 sed -i -e "${SED_HISTORY_RC}" ${TEST_ROOT}/${RUNDIR}/HISTORY.rc
 
 RUNDIR="gc_4x5_benchmark_merra2"
 CMD="1\n1\n2\n1\n1\n${TEST_ROOT}\n${RUNDIR}\nn\n"
 printf ${CMD} | ./createRunDir.sh >> ${LOG} 2>&1
-sed -i -e "${SED_INPUT_GEOS}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_1}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_2}" ${TEST_ROOT}/${RUNDIR}/input.geos
 sed -i -e "${SED_HISTORY_RC}" ${TEST_ROOT}/${RUNDIR}/HISTORY.rc
 
 RUNDIR="gc_4x5_CH4_merra2"
 CMD="3\n1\n1\n1\n${TEST_ROOT}\n${RUNDIR}\nn\n"
 printf ${CMD} | ./createRunDir.sh >> ${LOG} 2>&1
-sed -i -e "${SED_INPUT_GEOS}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_1}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_2}" ${TEST_ROOT}/${RUNDIR}/input.geos
 sed -i -e "${SED_HISTORY_RC}" ${TEST_ROOT}/${RUNDIR}/HISTORY.rc
 
 RUNDIR="gc_4x5_CO2_merra2"
 CMD="4\n1\n1\n1\n${TEST_ROOT}\n${RUNDIR}\nn\n"
 printf ${CMD} | ./createRunDir.sh >> ${LOG} 2>&1
-sed -i -e "${SED_INPUT_GEOS}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_1}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_2}" ${TEST_ROOT}/${RUNDIR}/input.geos
 sed -i -e "${SED_HISTORY_RC}" ${TEST_ROOT}/${RUNDIR}/HISTORY.rc
 
 RUNDIR="gc_4x5_Hg_merra2"
 CMD="5\n1\n1\n1\n${TEST_ROOT}\n${RUNDIR}\nn\n"
 printf ${CMD} | ./createRunDir.sh >> ${LOG} 2>&1
-sed -i -e "${SED_INPUT_GEOS}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_1}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_2}" ${TEST_ROOT}/${RUNDIR}/input.geos
 sed -i -e "${SED_HISTORY_RC}" ${TEST_ROOT}/${RUNDIR}/HISTORY.rc
 
 #RUNDIR="gc_4x5_POPs_merra2"
 #CMD="6\n1\n1\n1\n${TEST_ROOT}\n${RUNDIR}\nn\n"
 #printf ${CMD} | ./createRunDir.sh >> ${LOG} 2>&1
-#sed -i -e "${SED_INPUT_GEOS}" ${TEST_ROOT}/${RUNDIR}/input.geos
+#sed -i -e "${SED_INPUT_GEOS_1}" ${TEST_ROOT}/${RUNDIR}/input.geos
+#sed -i -e "${SED_INPUT_GEOS_2}" ${TEST_ROOT}/${RUNDIR}/input.geos
 #sed -i -e "${SED_HISTORY_RC}" ${TEST_ROOT}/${RUNDIR}/HISTORY.rc
 
 RUNDIR="gc_4x5_tagCH4_merra2"
 CMD="7\n1\n1\n1\n${TEST_ROOT}\n${RUNDIR}\nn\n"
 printf ${CMD} | ./createRunDir.sh >> ${LOG} 2>&1
-sed -i -e "${SED_INPUT_GEOS}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_1}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_2}" ${TEST_ROOT}/${RUNDIR}/input.geos
 sed -i -e "${SED_HISTORY_RC}" ${TEST_ROOT}/${RUNDIR}/HISTORY.rc
 
 RUNDIR="gc_4x5_tagCO_merra2"
 CMD="8\n1\n1\n1\n${TEST_ROOT}\n${RUNDIR}\nn\n"
 printf ${CMD} | ./createRunDir.sh >> ${LOG} 2>&1
-sed -i -e "${SED_INPUT_GEOS}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_1}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_2}" ${TEST_ROOT}/${RUNDIR}/input.geos
 sed -i -e "${SED_HISTORY_RC}" ${TEST_ROOT}/${RUNDIR}/HISTORY.rc
 
 RUNDIR="gc_4x5_tagO3_merra2"
 CMD="9\n1\n1\n1\n${TEST_ROOT}\n${RUNDIR}\nn\n"
 printf ${CMD} | ./createRunDir.sh >> ${LOG} 2>&1
-sed -i -e "${SED_INPUT_GEOS}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_1}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_2}" ${TEST_ROOT}/${RUNDIR}/input.geos
 sed -i -e "${SED_HISTORY_RC}" ${TEST_ROOT}/${RUNDIR}/HISTORY.rc
 
 RUNDIR="gc_4x5_TransportTracers_merra2"
 CMD="10\n1\n1\n1\n${TEST_ROOT}\n${RUNDIR}\nn\n"
 printf ${CMD} | ./createRunDir.sh >> ${LOG} 2>&1
-sed -i -e "${SED_INPUT_GEOS}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_1}" ${TEST_ROOT}/${RUNDIR}/input.geos
+sed -i -e "${SED_INPUT_GEOS_2}" ${TEST_ROOT}/${RUNDIR}/input.geos
 sed -i -e "${SED_HISTORY_RC}" ${TEST_ROOT}/${RUNDIR}/HISTORY.rc
 
 
